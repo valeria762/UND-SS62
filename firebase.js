604757dbf3d22e64a4b6fdd3a2d25d5cc3fdcdc2
valeria <valeriamendes409@gmail.com>
2024-10-14 19:24:00 -0500

    agregando archivos

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
+  import { } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCCPWL46zQSdHqJTbryvzHbKZ4WCRb7HPE",
+    authDomain: "login-p-a771a.firebaseapp.com",
+    projectId: "login-p-a771a",
+    storageBucket: "login-p-a771a.appspot.com",
+    messagingSenderId: "773498899926",
+    appId: "1:773498899926:web:1a650e4a5270194380e877",
+    measurementId: "G-LB8BXC0RPF"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
